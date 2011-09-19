af1e8f7dca4d790032f2dc7b1dfaf14c3cbb21b0
Michael Wells <michaelwells02@hotmail.com>
2011-09-19 12:33:58 -0400

    Added error handling for start.bat

--- Start.bat
+++ Start.bat
@@ -5,3 +5,9 @@ rem echo "%curr_path%"
 echo Opening the Start-SqlSpade script using PowerShell ISE 
 
 powershell.exe -NoProfile "start-process PowerShell_ISE.exe -argumentlist %curr_path% -verb RunAs"
+
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+
+:Error
+echo Unable to open Start-SqlSpade.ps1 - please verify that the file is present and that the PowerShell ISE is installed
+pause
