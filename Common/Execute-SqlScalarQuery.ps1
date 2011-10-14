3eb122a4c818102d6e129e64e95c90b190456460
Michael Wells <michaelwells02@hotmail.com>
2011-10-14 11:06:59 -0400

    Added the Denali template and a common function that can execute scalar queries

---
+++ Common/Execute-SqlScalarQuery.ps1
@@ -0,0 +1,38 @@
+﻿Function Execute-SqlScalarQuery
+{
+	param
+	( 
+		[Parameter(Position=0, Mandatory=$true)] [string] $sqlScript,
+		[Parameter(Position=1, Mandatory=$true)] [AllowEmptyString()] [string] $sqlInstance,
+        [Parameter(Position=2, Mandatory=$false)] [string] $serverName = ".",
+        [Parameter(Position=3, Mandatory=$false)] [string] $databaseName = "master"
+	)
+    
+	$conn = new-Object System.Data.SqlClient.SqlConnection("Server=$serverName\$sqlInstance;DataBase=$databaseName;Integrated Security=SSPI;")
+	$sqlCommand = New-Object System.Data.SqlClient.SqlCommand
+	$sqlCommand.Connection = $conn
+	$sqlCommand.CommandType = [System.Data.CommandType]'Text'
+	$sqlCommand.CommandTimeout = 300
+	
+	try
+	{
+		$conn.Open() | out-null	
+		
+		$sqlCommand.CommandText = $sqlScript
+		[int] $retVal = $sqlCommand.ExecuteScalar()
+		
+        $strResult = "Command(s) completed successfully."
+	}
+	catch [System.Exception]
+	{
+		$strResult = $_
+	}
+	finally
+	{
+		if ($conn.State -ne [System.Data.ConnectionState]'Closed')
+		{
+			$conn.Close()
+			return $retVal
+		}
+	}
+}
