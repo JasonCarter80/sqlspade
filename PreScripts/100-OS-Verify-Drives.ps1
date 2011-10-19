72e11e2575fbc164f04a91a7a3741010b56a1682
Michael Wells <SqlTechMike@users.noreply.github.com>
2011-10-19 16:22:33 -0400

    Added additional checks for FileSystem, Disk Alignment, and Compression - also added configuration settings to determine if warnings or errors should be thrown

--- PreScripts/100-OS-Verify-Drives.ps1
+++ PreScripts/100-OS-Verify-Drives.ps1
@@ -40,28 +40,114 @@
 
 $configParams = $args[0]
 
-$filePath = $configParams["FilePath"]
+$filePath 		= $configParams["FilePath"]
+$scriptFolder 	= $configParams["WindowsScriptsFolder"]
+$dbaDrive 		= $filePath.Substring(0,2)
+$scriptsDrive	= $scriptFolder.Substring(0,2)
 
-$dbaDrive = $filePath.Substring(0,2)
+#Load the script parameters from the config file
+[array] $nodes = ($Global:ScriptConfigs | ?{$_.Name -eq "Verify-Drives"}).SelectNodes("Param")
+$paramFailOnFileSystem = ($nodes | ? {$_.Name -eq "FailOnFileSystem"}).Value
+$paramFailOnDiskAlign = ($nodes | ? {$_.Name -eq "FailOnDiskAlign"}).Value
+$paramFailOnCompressed = ($nodes | ? {$_.Name -eq "FailOnCompressed"}).Value
 
-[array] $drives = gwmi win32_logicaldisk | where {$_.DriveType -eq 3 -or $_.Drive.Type -eq 4}
+[array] $missing = $nodes | ? {$_.Value -eq ""}
+if ($missing.Count -gt 0)
+{
+	return "Script not executed: please check the Run-Install.config file for missing configuration items"
+}
+
+# [array] $drives = gwmi win32_logicaldisk | where {$_.DriveType -eq 3 -or $_.Drive.Type -eq 4}
+# foreach($drive in $drives)
+# {
+    # $path = $drive.DeviceID + "\test_file.txt"
+    # New-Item $path -itemType File | out-null
+    # Write-Log -level "Info" -message "Test file written - $path"
+    # Remove-Item $path
+    # Write-Log -level "Info" -message "Test file deleted - $path"
+    
+    # if($drive.DeviceID -eq "C:")
+    # {
+    	# Create-Folder 'C:\Windows\Script'
+    # }
+	
+	# if($drive.DeviceID -eq $dbaDrive)
+    # {
+		# Create-Folder "$dbaDrive\Tools" 
+		# Create-Folder "$dbaDrive\Tools\Install" 
+		# Create-Folder "$dbaDrive\sqlrec" 
+    # }
+# }
+
+[array] $drives = gwmi win32_Volume | where {$_.DriveType -eq 3 -or $_.Drive.Type -eq 4}
 foreach($drive in $drives)
 {
-    $path = $drive.DeviceID + "\test_file.txt"
+    #Check File System
+    if ($drive.FileSystem -ne "NTFS")
+    {
+        if ($paramFailOnFileSystem -eq 1)
+		{
+			write-log -level "Error" -message "The $($drive.DriveLetter) drive is formatted as $($drive.FileSystem)"
+		}
+		else
+		{
+			write-log -level "Warning" -message "The $($drive.DriveLetter) drive is formatted as $($drive.FileSystem)"
+		}
+    }
+    else
+    {
+        write-log -level "Info" -message "The $($drive.DriveLetter) drive is formatted as $($drive.FileSystem)"
+    }
+    
+    #Check disk alignment
+    if ($drive.BlockSize -ne "4096")
+    {
+        if ($paramFailOnDiskAlign -eq 1)
+		{
+			write-log -level "Error" -message "The $($drive.DriveLetter) drive does not appear to be disk aligned - Block Size:$($drive.BlockSize)"
+		}
+		else
+		{
+			write-log -level "Warning" -message "The $($drive.DriveLetter) drive does not appear to be disk aligned - Block Size:$($drive.BlockSize)"
+		}
+    }
+    else
+    {
+        write-log -level "Info" -message "The $($drive.DriveLetter) drive appears to be disk aligned - Block Size:$($drive.BlockSize)"
+    }
+    
+    #Check disk compression
+    if ($drive.Compressed -eq $true)
+    {
+        if ($paramFailOnCompressed -eq 1)
+		{
+			write-log -level "Error" -message "The $($drive.DriveLetter) drive is compressed - SQL Server cannot use a compressed drive"
+		}
+		else
+		{
+			write-log -level "Warning" -message "The $($drive.DriveLetter) drive is compressed - SQL Server cannot use a compressed drive"
+		}
+    }
+    else
+    {
+        write-log -level "Info" -message "The $($drive.DriveLetter) drive is not compressed"
+    }
+
+    $path = $drive.DriveLetter + "\test_file.txt"
     New-Item $path -itemType File | out-null
     Write-Log -level "Info" -message "Test file written - $path"
     Remove-Item $path
     Write-Log -level "Info" -message "Test file deleted - $path"
     
-    if($drive.DeviceID -eq "C:")
+    if($drive.DriveLetter -eq $scriptsDrive)
     {
-    	Create-Folder 'C:\Windows\Script'
+    	Create-Folder $scriptFolder
     }
 	
-	if($drive.DeviceID -eq $dbaDrive)
+	if($drive.DriveLetter -eq $dbaDrive)
     {
-		Create-Folder "$dbaDrive\Tools" 
-		Create-Folder "$dbaDrive\Tools\Install" 
+		Create-Folder "$filePath" 
+		Create-Folder "$filePath\Install" 
 		Create-Folder "$dbaDrive\sqlrec" 
     }
 }
