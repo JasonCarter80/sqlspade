5c86cadc159a89240afba10f3ae0f637527fe779
Jason Carter <JasonCarter80@users.noreply.github.com>
2016-08-23 19:36:22 -0400

    Moving nonstandard Pre/Post scripts to the Examples folder and removing hard coding to them

--- Start-SqlSpade.ps1
+++ Start-SqlSpade.ps1
@@ -7,35 +7,21 @@ $path = Join-Path (Split-Path $Invocation.MyCommand.Path) Run-Install.ps1
 . $path
 
 [hashtable] $ht = New-Object hashtable
-
-#Required Parameters
-$ht.Add("SqlVersion", 'Sql2012') #Valid values - Sql2005, Sql2008, Sql2008R2, Sql2012
-$ht.Add("SqlEdition", 'Developer') #Valid values - Standard, Enterprise, Developer (2008R2 and 2012 only)
-$ht.Add("sqlServiceAccount", 'SQLFTW\SQLService')
-$ht.Add("sqlServicePassword", 'SQL4tw!')
-$ht.Add("SysAdminPassword", 'SQL4tw!')
-$ht.Add("FilePath", 'S:\Tools')
-$ht.Add("DataCenter", 'Data Center 1')
-
-#Optional Parameters
-#$ht.Add("ProcessorArch", '')
-#$ht.Add("Debug", 'True')
-#$ht.Add("Simulation", 'True')
-
-#Custom Parameters - (Required)
-$ht.Add("DbaTeam", 'DBA-SQL')
-$ht.Add("InstanceName", 'SQLDEV2')
-$ht.Add("ProductStringName", 'Default')
-$ht.Add("Environment", 'Prod') #Valid values - Dev, QA, BCP, PreProd, Prod
-
 [hashtable] $overrides = New-Object hashtable
 
-#Optional values that are used directly in the generation 
-#of the configuration INI file.  Anything specified here 
-#will superceed values specified in the XML config file
+#Required Parameters
+$ht.Add("DataCenter", 'Data Center 1')           #Must be setup in Data Center node of Config
+$ht.Add("SqlVersion", 'Sql2012')                 #Must be setup in SQLVersions node of Config
+$ht.Add("SqlEdition", 'Developer')               #Must be setup in SQLVersions/Editions node of Config
+$ht.Add("SysAdminPassword", 'SQL4tw!')           #Must be STRONG PASSWOD  TODO:  Test for Strong Password
 
-#for a complete list of parameters please review the BOL topic
-#for installing SQL Server from the command line
+#Optional regularly used Parameters
+#$ht.Add("sqlServiceAccount", 'SQLFTW\SQLService')
+#$ht.Add("sqlServicePassword", 'SQL4tw!')
+#$ht.Add("InstanceName", 'SQLDEV2')              #Will default to the default instance if not provided
+#$ht.Add("ProductStringName", 'Default')         #Will default to 'DEFAULT' which must be setup in each SQLVERSION/ProductStrings node
+#$ht.Add("Environment", 'Prod')                   #Will default to PROD, can be used in Pre/Post scripts, not used by Installer
+
 
 #$overrides.Add("SQLBACKUPDIR", 'C:\$Recycle.Bin')
 #$overrides.Add("SQLUSERDBDIR", 'H:\MSSQL10\MSSQL\Data')
