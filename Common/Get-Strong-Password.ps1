9245609278b3178353338bc87ba2fa4682bc5b3c
Jason Carter <JasonCarter80@users.noreply.github.com>
2016-08-24 14:02:21 -0400

    SA Password is now optional, will be generated as needed.  Also testing for Strong Password per SQL Install Requirements

---
+++ Common/Get-Strong-Password.ps1
@@ -0,0 +1,10 @@
+﻿Function Get-Strong-Password() {
+    Param(
+        [int]$length=12
+    )
+
+    $a = [Reflection.Assembly]::LoadWithPartialName("System.Web") 
+    $([System.Web.Security.Membership]::GeneratePassword($length,$length/4))
+}
+
+
