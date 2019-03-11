409f9a46e33f76cee76b971dcd640b696c78449e
Greg Buczylowski <gbuczylowski@gmail.com>
2019-03-11 09:59:25 +0000

    added a new post-script

--- Examples/PostScripts/600-Rename-Disable-SA.sql
+++ Examples/PostScripts/600-Rename-Disable-SA.sql
@@ -5,3 +5,27 @@
  *----------------------------------------------------------------------------------------------------
  * Description: Rename & disable built-in sa account
  *****************************************************************************************************/
+
+ /*rename sa*/
+if exists
+(
+    select name
+         , sid
+    from sys.server_principals as sp
+    where sid = 0x01
+          and name = 'sa'
+)
+begin
+    alter login [sa] with name = [usa];
+end;
+/*and disable the account*/
+if exists
+(
+    select name
+         , sid, *
+    from sys.server_principals as sp
+    where sid = 0x01 and sp.is_disabled=0          
+)
+begin
+    alter login [usa] disable
+end
