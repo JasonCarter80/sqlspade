969d74d24ee97afa5a1ffea465f276d70e401872
Jason Carter <jason@jason-carter.net>
2022-10-11 17:08:00 -0400

    Cleanup

--- PreScripts/100-OS-Verify-NetFramework.ps1
+++ PreScripts/100-OS-Verify-NetFramework.ps1
@@ -40,7 +40,7 @@
 
 $configParams = $args[0]
 	
-#Verify .NetFramework 3.5 SP1 is installed: http://serverinfo.rjf.com/  key in server name, click update
+#Verify .NetFramework 3.5 SP1 is installed
 $framework = Get-ItemProperty -path "HKLM:\Software\Microsoft\NET Framework Setup\ndp\v3.5" -name "SP"
 if($framework.SP -ge 1)
 {
